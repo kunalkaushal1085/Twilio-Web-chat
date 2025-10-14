b2200069f969509cc65eb3cd9a0ebba2111bf68c
Kunal Kaushal <kunal1085@gmail.com>
2025-10-14 17:42:18 +0530

    port change in app.py file

--- sms_service/app.py
+++ sms_service/app.py
@@ -562,7 +562,7 @@ async def get_openai_response(chat_history: List[SMSMessageSchema], system_promp
 if __name__ == "__main__":
     with app.app_context():
         db.create_all()
-    app.run(host="0.0.0.0",port=8000, debug=True)
+    app.run(host="0.0.0.0",port=8100, debug=True)
 
 
 
